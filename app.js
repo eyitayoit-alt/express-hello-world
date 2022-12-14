7e0eca06f7d5d7b3ab6534d94ef11fdd87e04024
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2022-12-14 16:34:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const path = require("path");
-const app = express()
+const app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var passport = require('passport');
+var index = require('./routes/index');
+var authIndex = require('./routes/auth');
+
 
 // #############################################################################
 // Logs all request paths and method
@@ -39,5 +46,39 @@ app.use('*', (req,res) => {
     })
     .end()
 })
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/', index);
+app.use('/', authIndex);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
 
 module.exports = app
